79247560238996f36c7bc841ce9dc4fa178c0a61
Pedro Henrique <pedro.souza@fator3tech.com.br>
2024-12-24 18:27:40 -0300

    fix: new lib versions and riot api adaptation

--- Telas/Historico.js
+++ Telas/Historico.js
@@ -1,26 +1,31 @@
 import { useEffect, useState } from "react";
-import { Text, View, StyleSheet, Image, FlatList, SafeAreaView } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  FlatList,
+  SafeAreaView,
+} from "react-native";
 import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
 import { getPartidas } from "../Tools/Partidas";
 import { colors } from "../Style/Colors";
 import { StatusBar } from "expo-status-bar";
-import { ActivityIndicator } from 'react-native-paper';
+import { ActivityIndicator } from "react-native-paper";
 
 export default function Historico({ route, navigation }) {
+  const [partidas, setPartidas] = useState([]);
+  const invocador = route.params.invocador;
 
-    const [partidas, setPartidas] = useState([]);
-    const invocador = route.params.invocador;
+  useEffect(() => {
+    getPartidas(invocador.puuid).then((partidas) => {
+      setPartidas(partidas);
+    });
+  }, []);
 
-    useEffect(() => {
-        getPartidas(invocador.puuid).then((partidas) => {
-            setPartidas(partidas);
-        });
-    }, []);
-
-    return (
-        <SafeAreaView style={[styles.AndroidSafeArea, styles.container]}>
-
-            {/* {partidas.length > 0 ? (
+  return (
+    <SafeAreaView style={[styles.AndroidSafeArea, styles.container]}>
+      {/* {partidas.length > 0 ? (
                 <TouchableOpacity
                     onPress={() => navigation.pop()}
                     style={styles.voltar}
@@ -31,195 +36,211 @@ export default function Historico({ route, navigation }) {
                 </TouchableOpacity>
             ) : null} */}
 
-            <ScrollView
-                contentContainerStyle={styles.scrollView}
-            >
-                {
-                    partidas.length > 0 ? partidas.map((partida) => {
+      <ScrollView contentContainerStyle={styles.scrollView}>
+        {partidas.length > 0 ? (
+          partidas.map((partida) => {
+            const styleWin = partida.me.win ? styles.win : styles.lose;
 
-                        const styleWin = partida.me.win ? styles.win : styles.lose;
+            return (
+              <View style={[styleWin, styles.partida]} key={partida.gameId}>
+                <View>
+                  <Text style={[styles.gamemode, styles.textWhite]}>
+                    {partida.gameMode}
+                  </Text>
+                  <Text
+                    style={[styles.textWhite, styles.frag]}
+                  >{`${partida.me.kills}/${partida.me.deaths}/${partida.me.assists}`}</Text>
+                </View>
+
+                <View>
+                  <Image
+                    style={styles.championImg}
+                    source={{ uri: partida.me.championImg }}
+                  />
+                </View>
+
+                <View style={[styles.spells]}>
+                  {partida.me.spells.map((s, index) => {
+                    return (
+                      <Image
+                        style={styles.spellImg}
+                        key={index}
+                        source={{ uri: s }}
+                      />
+                    );
+                  })}
+                </View>
+
+                <View style={[styles.othersView]}>
+                  <Text
+                    style={[styles.textWhite, styles.other]}
+                  >{`Nível: ${partida.me.champLevel}`}</Text>
+                  <Text
+                    style={[styles.textWhite, styles.other]}
+                  >{`Farm: ${partida.me.totalMinionsKilled}`}</Text>
+                </View>
+
+                <View style={[styles.buildView]}>
+                  <View style={[styles.buildLinha]}>
+                    {partida.me.build.map((item, index) => {
+                      if (index < 3) {
+                        if (item.endsWith("/0.png")) {
+                          return (
+                            <Image
+                              style={styles.itemVazio}
+                              key={index}
+                              source={{ uri: item }}
+                            />
+                          );
+                        }
 
                         return (
-                            <View style={[styleWin, styles.partida]} key={partida.gameId}>
+                          <Image
+                            style={styles.buildItem}
+                            key={index}
+                            source={{ uri: item }}
+                          />
+                        );
+                      }
+                    })}
+                  </View>
 
-                                <View>
-                                    <Text style={[styles.gamemode, styles.textWhite]}>{partida.gameMode}</Text>
-                                    <Text style={[styles.textWhite, styles.frag]}>{`${partida.me.kills}/${partida.me.deaths}/${partida.me.assists}`}</Text>
-                                </View>
+                  <View style={[styles.buildLinha]}>
+                    {partida.me.build.map((item, index) => {
+                      if (index >= 3 && index < 6) {
+                        if (item.endsWith("/0.png")) {
+                          return (
+                            <Image
+                              style={styles.itemVazio}
+                              key={index}
+                              source={{ uri: item }}
+                            />
+                          );
+                        }
+                        return (
+                          <Image
+                            style={styles.buildItem}
+                            key={index}
+                            source={{ uri: item }}
+                          />
+                        );
+                      }
+                    })}
+                  </View>
+                </View>
+              </View>
+            );
+          })
+        ) : (
+          <ActivityIndicator animating={true} color={colors.white} />
+        )}
+      </ScrollView>
 
-                                <View>
-                                    <Image style={styles.championImg} source={{ uri: partida.me.championImg }} />
-                                </View>
-
-                                <View style={[styles.spells]}>
-                                    {
-                                        partida.me.spells.map((s, index) => {
-                                            return (
-                                                <Image style={styles.spellImg} key={index} source={{ uri: s }} />
-                                            )
-                                        })
-                                    }
-                                </View>
-
-                                <View style={[styles.othersView]}>
-                                    <Text style={[styles.textWhite, styles.other]}>{`Nível: ${partida.me.champLevel}`}</Text>
-                                    <Text style={[styles.textWhite, styles.other]}>{`Farm: ${partida.me.totalMinionsKilled}`}</Text>
-                                </View>
-
-                                <View style={[styles.buildView]}>
-
-                                    <View style={[styles.buildLinha]}>
-                                        {
-                                            partida.me.build.map((item, index) => {
-                                                if (index < 3) {
-                                                    if (item.endsWith("/0.png")) {
-                                                        return (
-                                                            <Image style={styles.itemVazio} key={index} source={{ uri: item }} />
-                                                        )
-                                                    }
-
-                                                    return (
-                                                        <Image style={styles.buildItem} key={index} source={{ uri: item }} />
-                                                    )
-                                                }
-                                            })
-                                        }
-                                    </View>
-
-                                    <View style={[styles.buildLinha]}>
-                                        {
-                                            partida.me.build.map((item, index) => {
-                                                if (index >= 3 && index < 6) {
-                                                    if (item.endsWith("/0.png")) {
-                                                        return (
-                                                            <Image style={styles.itemVazio} key={index} source={{ uri: item }} />
-                                                        )
-                                                    }
-                                                    return (
-                                                        <Image style={styles.buildItem} key={index} source={{ uri: item }} />
-                                                    )
-                                                }
-                                            })
-                                        }
-                                    </View>
-
-                                </View>
-
-                            </View>
-                        )
-                    })
-
-                        : <ActivityIndicator animating={true} color={colors.white} />
-                }
-            </ScrollView>
-
-            <StatusBar style="light" />
-        </SafeAreaView>
-    )
+      <StatusBar style="light" />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    AndroidSafeArea: {
-        flex: 1,
-        backgroundColor: "white",
-        paddingTop: Platform.OS === "android" ? 30 : 0
-    },
-    container: {
-        flex: 1,
-        backgroundColor: colors.primary,
-        justifyContent: "center",
-        alignItems: "center",
-
-    },
-    scrollView: {
-        width: "100%",
-        justifyContent: "center",
-    },
-    botao: {
-        alignItems: "center",
-        paddingHorizontal: 20,
-        paddingVertical: 10,
-        borderRadius: 10,
-        borderWidth: 1,
-        borderColor: colors.white,
-        marginBottom: 15,
-    },
-    loading: {
-        margin: "auto",
-        fontSize: 20,
-
-    },
-    partida: {
-        padding: 10,
-        flexDirection: 'row',
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: 10,
-        borderRadius: 10,
-        marginBottom: 10
-    },
-    win: {
-        backgroundColor: colors.darkGreen,
-    },
-    lose: {
-        backgroundColor: colors.red,
-    },
-    gamemode: {
-        marginRight: 10,
-        fontSize: 12,
-    },
-    championImg: {
-        width: 50,
-        height: 50,
-        marginLeft: 10,
-    },
-    spellImg: {
-        width: 25,
-        height: 25,
-        marginBottom: 5,
-        borderRadius: 50
-    },
-    spells: {
-        marginLeft: 10,
-    },
-    othersView: {
-        marginLeft: 10,
-        height: "100%",
-    },
-    fragView: {
-        height: "100%",
-    },
-    frag: {
-        fontWeight: "bold",
-        marginTop: 10,
-        fontSize: 12,
-    },
-    other: {
-        fontWeight: "bold",
-        fontSize: 14
-    },
-    buildView: {
-        flexDirection: "column",
-        marginLeft: 10
-    },
-    buildLinha: {
-        flexDirection: "row",
-    },
-    buildItem: {
-        width: 30,
-        height: 30,
-        borderRadius: 5,
-    },
-    itemVazio: {
-        width: 29,
-        height: 29,
-        backgroundColor: colors.gray,
-        borderWidth: 1,
-        borderRadius: 5,
-        borderColor: colors.white,
-        opacity: 0.5,
-    },
-    textWhite: {
-        color: colors.white,
-    }
+  AndroidSafeArea: {
+    flex: 1,
+    backgroundColor: "white",
+    paddingTop: Platform.OS === "android" ? 30 : 0,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: colors.primary,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  scrollView: {
+    width: "100%",
+    justifyContent: "center",
+  },
+  botao: {
+    alignItems: "center",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: colors.white,
+    marginBottom: 15,
+  },
+  loading: {
+    margin: "auto",
+    fontSize: 20,
+  },
+  partida: {
+    padding: 10,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  win: {
+    backgroundColor: colors.darkGreen,
+  },
+  lose: {
+    backgroundColor: colors.red,
+  },
+  gamemode: {
+    marginRight: 10,
+    fontSize: 12,
+  },
+  championImg: {
+    width: 50,
+    height: 50,
+    marginLeft: 10,
+  },
+  spellImg: {
+    width: 25,
+    height: 25,
+    marginBottom: 5,
+    borderRadius: 50,
+  },
+  spells: {
+    marginLeft: 10,
+  },
+  othersView: {
+    marginLeft: 10,
+    height: "100%",
+  },
+  fragView: {
+    height: "100%",
+  },
+  frag: {
+    fontWeight: "bold",
+    marginTop: 10,
+    fontSize: 12,
+  },
+  other: {
+    fontWeight: "bold",
+    fontSize: 14,
+  },
+  buildView: {
+    flexDirection: "column",
+    marginLeft: 10,
+  },
+  buildLinha: {
+    flexDirection: "row",
+  },
+  buildItem: {
+    width: 30,
+    height: 30,
+    borderRadius: 5,
+  },
+  itemVazio: {
+    width: 29,
+    height: 29,
+    backgroundColor: colors.gray,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: colors.white,
+    opacity: 0.5,
+  },
+  textWhite: {
+    color: colors.white,
+  },
 });
--- Tools/User/index.js
+++ Tools/User/index.js
@@ -3,20 +3,32 @@ import { headers } from "../Headers/index.js";
 import { getLastVersion } from "../Version/index.js";
 
 export const getUserDataByName = async (user) => {
+  const formatedString = user.split(" ").join("+");
+  const lastVersion = await getLastVersion();
 
-    const formatedString = user.split(" ").join("%20");
-    const lastVersion = await getLastVersion();
+  try {
+    const { data } = await axios.get(
+      `https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${formatedString}/BR1`,
+      { headers }
+    );
 
-    try {
-        var { data } = await axios.get(`https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${formatedString}`, { headers });
-        data.profileIconImg = `http://ddragon.leagueoflegends.com/cdn/${lastVersion}/img/profileicon/${data.profileIconId}.png`;
- 
-        return data;
-    }
-    catch (error) {
-        console.log(error);
-        return undefined;
-    }
+    const puuid = data.puuid;
 
+    const { data: data2 } = await axios.get(
+      `https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}`,
+      { headers }
+    );
 
-}
+    const result = {
+      puuid: puuid,
+      profileIconImg: `http://ddragon.leagueoflegends.com/cdn/${lastVersion}/img/profileicon/${data2.profileIconId}.png`,
+      summonerLevel: data2.summonerLevel,
+      name: data.gameName,
+    };
+
+    return result;
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
+};
